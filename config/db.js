54e8658fd7b74894595e5b02fcdc620576914461
unknown <nasrullah_hussaini@yahoo.com>
2021-08-04 11:44:46 -0400

    connect to mongodb atlas

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+
+//connect to mongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true 
+    });
+
+    console.log('mongoDB connected ...');
+  }catch(err) {
+    console.error(err.message);
+
+    //exit process with failure
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
