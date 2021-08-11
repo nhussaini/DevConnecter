e21a410db246126724df55fa8c7ac7122114e71a
unknown <nasrullah_hussaini@yahoo.com>
2021-08-11 18:37:54 -0400

    create a new user to register

--- config/db.js
+++ config/db.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
-
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 //connect to mongoDB
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-      useNewUrlParser: true 
+      useNewUrlParser: true,
+      useCreateIndex: true,
     });
 
-    console.log('mongoDB connected ...');
-  }catch(err) {
+    console.log("mongoDB connected ...");
+  } catch (err) {
     console.error(err.message);
 
     //exit process with failure
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,11 @@ const express = require("express");
 //The express.Router() function is used to create a new router object.
 //This function is used when you want to create a new router object in your program to handle requests.
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+const User = require("../../models/User");
 
 // @route    POST api/users
 // @desc     Register User
@@ -22,11 +26,43 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //see if user exists
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      //get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      //create a new user
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+
+    //encrypt password
+    //return jsonwebtoken
     console.log(req.body);
   }
 );
