4ca64463e3b5bdaafbb61f292d7a0519f1ed3d72
unknown <nasrullah_hussaini@yahoo.com>
2021-08-22 17:13:51 -0400

    add Register component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>Create Your Account
+      </p>
+      <form action="dashboard.html" className="form">
+        <div className="form-group">
+          <input type="text" placeholder="Name" required />
+        </div>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" />
+          <small className="form-text">
+            This site uses Gravatar, so if you want a profile image, use a
+            gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input type="password" placeholder="Password" minlength="6" />
+        </div>
+        <div className="form-group">
+          <input type="password" placeholder="Confirm Password" minlength="6" />
+        </div>
+        <input type="submit" value="Register" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Register;
